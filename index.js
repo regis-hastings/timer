0b67928f32547d2a8e58f5b657f27f219c52ff16
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-05 14:26:29 -0700

    add pause and tick functions

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ class Timer {
 		this.pauseButton = pauseButton;
 
 		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
 	}
 
 	start = () => {
-		this.importantMethodToCall();
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
 	};
 
-	importantMethodToCall() {
-		console.log('Important thing was done!!!');
-	}
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	tick = () => {
+		console.log('tick');
+	};
 }
 
 const durationInput = document.querySelector('#duration');
