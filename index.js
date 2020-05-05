7c12ceee91753e4d8e6a799709a6f29cc4c5ca0c
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-05 13:47:27 -0700

    refactor start method to use arrow function

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ class Timer {
 		this.startButton.addEventListener('click', this.start);
 	}
 
-	start() {
-		console.log('Time to start the timer!');
+	start = () => {
+		this.importantMethodToCall();
+	};
+
+	importantMethodToCall() {
+		console.log('Important thing was done!!!');
 	}
 }
 
